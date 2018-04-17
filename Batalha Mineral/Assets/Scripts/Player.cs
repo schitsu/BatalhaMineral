45948dd3391bb97f946a5bae4ec25f5c726724b6
schitsu <38467556+schitsu@users.noreply.github.com>
2018-04-17 16:49:25 +0100

    JogoAdicionado

---
+++ Batalha Mineral/Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D myRigidbody;
+	private Animator myAnimator;
+
+	[SerializeField]
+	private float movementSpeed;
+	private bool attack;
+
+	private bool facingRight;
+
+
+    void Start()
+    {
+	facingRight = true;
+        myRigidbody = GetComponent<Rigidbody2D>();
+		myAnimator = GetComponent<Animator>();
+
+    }
+	void Update () 
+{
+	HandleInput();
+}
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        HandleMovement(horizontal);
+		Flip(horizontal);
+			HandleAttacks();
+				ResetValues();
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+	if 
+(!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+	{
+		 myRigidbody.velocity = new Vector2(horizontal * 					movementSpeed, myRigidbody.velocity.y);
+	}	
+		myAnimator.SetFloat("speed",Mathf.Abs(horizontal));
+
+    }
+	private void HandleAttacks ()
+{
+	if (attack && 
+!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+
+	{
+		myAnimator.SetTrigger("attack");
+		myRigidbody.velocity = Vector2.zero;
+	}
+}
+	private void HandleInput ()
+{
+		if (Input.GetKeyDown(KeyCode.LeftShift)) 
+		{
+			attack = true;
+		}
+}
+	private void Flip(float horizontal) 
+{	
+	if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) 
+		{
+	facingRight = !facingRight;
+
+	Vector3 theScale = transform.localScale;
+
+	theScale.x *= -1;
+
+	transform.localScale = theScale;
+		}
+}
+
+private void ResetValues()
+{
+	attack = false;
+}
+
+}
---
+++ Batalha Mineral/Assets/Scripts/Scripts/TImer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TImer : MonoBehaviour {
+	float timeRemaining = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeRemaining -= Time.deltaTime;
+	}
+	void OnGUI(){
+		if(timeRemaining > 0){
+			GUI.Label (new Rect( 100, 100, 200, 100), "Time Left: " + timeRemaining);
+		}
+		else {
+			GUI.Label(new Rect(100, 100, 100, 100), "Time is Upe"); 
+		}
+	}
+
+}
